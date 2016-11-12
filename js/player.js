f4f90efb86475e105c2526f41570033b4790e641
Eric Mofield <ericmofield@gmail.com>
2016-11-12 00:50:13 -0500

    update player sprite

--- js/player.js
+++ js/player.js
@@ -1,22 +1,13 @@
 function createPlayer() {
-    // The player and its settings
     player = game.add.sprite(0, 0, 'dude');
-    // player.scale.setTo(3, 3);
-    player.smoothed = false;
-
     player.position.setTo(game.world.centerX - player.width / 2, game.world.centerY - player.height / 2);
 
-
     player.maxSpeed = 100;
-    //  We need to enable physics on the player
     game.physics.arcade.enable(player);
     game.camera.follow(player);
-    //  Player physics properties. Give the little guy a slight bounce.
-    //player.body.bounce.y = 0.2;
-    //player.body.gravity.y = gravity;
+
     player.body.collideWorldBounds = true;
     player.anchor.setTo(0.5, 0.5);
-
 
     //  Our two animations, walking left and right.
     player.animations.add('down', [1, 2, 3, 0], 5, true);
@@ -60,17 +51,14 @@ function updatePlayer() {
         fireBullet();
     }
 
-
     //  Stand still
     if (!wasd.up.isDown && !wasd.down.isDown && !wasd.left.isDown && !wasd.right.isDown) {
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
         player.animations.stop();
         player.frame = 0;
     }
 
     //Rotate the player sprite to face the cursor
-    player.rotation = game.physics.arcade.angleToPointer(player);
+    //player.rotation = game.physics.arcade.angleToPointer(player);
 
 
     //Scratch code
